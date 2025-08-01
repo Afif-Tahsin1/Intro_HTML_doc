f4e2f7d7d233d4c1d1ef7e0616894fb9b0d84d15
Afif <jareen0512@gmail.com>
2025-08-01 14:23:05 +0600

    acp

---
+++ ACP/script.js
@@ -0,0 +1,11 @@
+let div = document.createElement("div");
+div.className = "alert";
+div.id = "paragraph";
+div.innerHTML = "<p>Congrats! you make a element in javascript";
+
+document.body.append(div);
+
+let heading = document.getElementById("heading");
+heading.innerHTML = "&#128512";
+
+document.querySelector("header p").innerHTML = `<b> Good job! </b>`;
